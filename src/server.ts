2b54775be88efeb6b805abc23ad165727b2bfcb8
nahinMSM <nahin_msm@hotmail.com>
2024-12-02 09:21:45 -0300

    adicionando correção de filmes duplicados

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,5 @@
 import express from 'express';
 import { PrismaClient } from '@prisma/client';
-// import { Request, Response } from 'express';
 
 const port = 3000;
 const app = express();
@@ -21,6 +20,17 @@ app.post('/movies', async (req, res) => {
   const { title, genre_id, language_id, oscar_count, release_date } = req.body;
 
   try {
+
+    const movieWithSameTitle = await prisma.movie.findFirst({
+      where: {
+        title: { equals: title, mode: 'insensitive' }
+      }
+    });
+
+    if (movieWithSameTitle) {
+      res.status(409).send({ message: 'Já existe um filme com esse título' });
+    }
+
     await prisma.movie.create({
       data: {
         title,
