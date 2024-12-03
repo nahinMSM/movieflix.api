25c5a2d001c975d44b578abf219affcdc03fe88d
nahinMSM <nahin_msm@hotmail.com>
2024-12-03 08:40:53 -0300

    adicionar filtro de filme por gênero

--- src/server.ts
+++ src/server.ts
@@ -88,6 +88,31 @@ app.delete('/movies/:id', async (req, res) => {
   res.status(200).send();
 });
 
+app.get('/movies/:genreName', async (req, res) => {
+  try {
+
+    const moviesFilteredByGenreName = await prisma.movie.findMany({
+      include: {
+        genres: true,
+        languages: true
+      },
+      where: {
+        genres: {
+          name: {
+            equals: req.params.genreName,
+            mode: 'insensitive'
+          }
+        }
+      }
+    });
+
+    res.status(200).send(moviesFilteredByGenreName);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ message: 'Falha ao atualizar um filme' });
+  };
+});
+
 app.listen(port, () => {
   console.log(`Servidor em axecução em http://localhost:${port}`);
 });
